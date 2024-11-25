96f950024c886a72a379cb3bbb4e4340fb7bc4ad
jyoti786J <160516610+jyoti786J@users.noreply.github.com>
2024-11-25 21:34:04 +0800

    Update student.js

--- models/student.js
+++ models/student.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const studentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    email: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Student', studentSchema);
